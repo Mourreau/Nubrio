0c5186d64978f9f8a0f0afad204b558863661e40
m.moreau <max.mourreau@yandex.ru>
2025-09-22 21:41:15 +0500

    feat(app): Добавить DTO для внешнего API - Open Meteo (#5)

---
+++ Nubrio.Application/DTOs/WeatherByDateResponseDto.cs
@@ -0,0 +1,13 @@
+namespace Nubrio.Application.DTOs;
+
+public class WeatherByDateResponseDto
+{
+    public string City { get; set; }
+    public DateOnly Date { get; set; }
+    public string Condition { get; set; }
+    public double Temperature { get; set; }
+    public string IconUrl { get; set; } 
+    public string Source { get; set; }
+    public DateTimeOffset FetchedAt{ get; set; }
+}
+
