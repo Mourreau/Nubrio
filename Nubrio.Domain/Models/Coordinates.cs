1e876a02c8d687624c5b30a7a21b935b76c9e2d2
m.moreau <max.mourreau@yandex.ru>
2025-09-23 18:24:44 +0500

    feat(dom): Реализация класса координат (#2)

---
+++ Nubrio.Domain/Models/Coordinates.cs
@@ -0,0 +1,52 @@
+namespace Nubrio.Domain.Models;
+
+public record Coordinates
+{
+    public Coordinates(double latitude, double longitude)
+    {
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+    
+    private double _latitude; // Широта
+    private double _longitude; // Долгота
+    
+    public double Latitude 
+    { 
+        get => _latitude;
+        init
+        {
+            if (!ValidateRange(-90, 90, value)) // Широта должна быть в диапазоне от -90 до 90 градусов
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(Latitude), 
+                    value, 
+                    "Latitude must be between -90 and 90.");
+            }
+
+            _latitude =  value;
+        }
+    }
+
+    public double Longitude
+    {
+        get => _longitude;
+        init
+        {
+            if (!ValidateRange(-180, 180, value)) // Долгота должна быть в диапазоне от -180 до 180 градусов
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(Longitude), 
+                    value, 
+                    "Longitude must be between -180 and 180.");
+            } 
+            
+            _longitude = value;
+        }
+    }
+
+    private bool ValidateRange(double min, double max, double value)
+    {
+        return value >= min && value <= max;
+    }
+}
