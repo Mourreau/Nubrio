9ae2938f552931f219368ef7329617d15a62365b
m.moreau <max.mourreau@yandex.ru>
2025-10-02 20:17:47 +0500

    feat(infra): Реализовать класс для мапинга кодов погоды WeatherCodeMappings (#2).

---
+++ Nubrio.Infrastructure/OpenMeteo/WeatherCodeMappings.cs
@@ -0,0 +1,18 @@
+namespace Nubrio.Infrastructure.OpenMeteo;
+
+public class WeatherCodeMappings
+{
+    public int[] Clear { get; set; }
+    public int[] PartlyCloudy { get; set; }
+    public int[] Cloudy { get; set; }
+    public int[] Fog { get; set; }
+    public int[] Drizzle { get; set; }
+    public int[] LightRain { get; set; }
+    public int[] Rain { get; set; }
+    public int[] HeavyRain { get; set; }
+    public int[] LightSnow { get; set; }
+    public int[] Snow { get; set; }
+    public int[] HeavySnow { get; set; }
+    public int[] Hailstorm { get; set; }
+    public int[] Thunderstorm { get; set; }
+}
