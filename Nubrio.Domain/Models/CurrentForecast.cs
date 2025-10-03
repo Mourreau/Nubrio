8d71d13843bdcd2f1cf52c1107124cae06b2b8d2
m.moreau <max.mourreau@yandex.ru>
2025-10-03 10:53:49 +0500

    refactor(dom): Произвести рефакторинг CurrentForecast (#4):
    - Добавить конструктор доменной модели;
    - Убрать сущность Location и оставить только LocationId

--- Nubrio.Domain/Models/CurrentForecast.cs
+++ Nubrio.Domain/Models/CurrentForecast.cs
@@ -3,10 +3,14 @@ using Nubrio.Domain.Enums;
 
 namespace Nubrio.Domain.Models;
 
-public class CurrentForecast
+public class CurrentForecast(
+    DateOnly date,
+    Guid locationId,
+    double temperature,
+    WeatherConditions conditions)
 {
-    public DateOnly Date { get; set; }
-    public Location ForecastLocation { get; set; }
-    public double Temperature { get; set; }
-    public WeatherConditions Conditions { get; set; }
+    public DateOnly Date { get; } = date;
+    public Guid LocationId { get; } = locationId;
+    public double Temperature { get; } = temperature;
+    public WeatherConditions Conditions { get; } = conditions;
 }
