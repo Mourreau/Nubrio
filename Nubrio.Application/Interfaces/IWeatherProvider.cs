b9d8ad086e23e710b40c7c9fc2adc1cd1463d288
m.moreau <max.mourreau@yandex.ru>
2025-10-06 18:40:34 +0500

    refactor(app): Изменить параметры метода IWeatherProvider.GetCurrentForecastAsync() (#5):
    - Теперь провайдер принимает координаты вместо города.

--- Nubrio.Application/Interfaces/IWeatherProvider.cs
+++ Nubrio.Application/Interfaces/IWeatherProvider.cs
@@ -5,7 +5,8 @@ namespace Nubrio.Application.Interfaces;
 
 public interface IWeatherProvider
 {
-    Task<Result<DailyForecast>> GetDailyForecastByDate(Coordinates coordinates, DateOnly startDate, DateOnly endDate);
-    Task<Result<CurrentForecast>> GetCurrentForecast(string city);
+    Task<Result<DailyForecast>> GetDailyForecastByDateAsync(
+        Coordinates coordinates, DateOnly startDate, DateOnly endDate, CancellationToken cancellationToken);
+    Task<Result<CurrentForecast>> GetCurrentForecastAsync(Coordinates coordinates, CancellationToken cancellationToken);
     
 }
