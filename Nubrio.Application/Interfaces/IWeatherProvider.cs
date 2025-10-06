2781e21265cc5e00f7ad8ed27b7beea58d87c5f5
m.moreau <max.mourreau@yandex.ru>
2025-10-06 16:33:09 +0500

    refactor(app): Убрать try-out из IWeatherProvider (#5).

--- Nubrio.Application/Interfaces/IWeatherProvider.cs
+++ Nubrio.Application/Interfaces/IWeatherProvider.cs
@@ -1,15 +1,11 @@
 using FluentResults;
-using Nubrio.Application.DTOs.CurrentForecast;
-using Nubrio.Application.DTOs.DailyForecast;
 using Nubrio.Domain.Models;
 
 namespace Nubrio.Application.Interfaces;
 
 public interface IWeatherProvider
 {
-    Task<Result<bool>> TryGetDailyForecastByDate(Coordinates coordinates, DateOnly startDate, DateOnly endDate,
-        out DailyResponseDto dailyForecast);
-    
-    Task<Result<bool>> TryGetCurrentForecast(string city, out CurrentResponseDto currentForecast);
+    Task<Result<DailyForecast>> GetDailyForecastByDate(Coordinates coordinates, DateOnly startDate, DateOnly endDate);
+    Task<Result<CurrentForecast>> GetCurrentForecast(string city);
     
 }
