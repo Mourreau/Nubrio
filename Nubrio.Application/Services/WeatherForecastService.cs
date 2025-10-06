c2ae0a8b5e751495d5e44d48fe19030d373bc4a6
m.moreau <max.mourreau@yandex.ru>
2025-10-06 20:36:29 +0500

    refactor(app): Произвести рефакториг WeatherForecastService (#5):
    - Добавлен IClock и IConditionStringMapper;
    - Добавлена проверка IsNullOrWhiteSpace для города;
    - Свойство CurrentForecastDto.FetchedAt теперь получается из IClock;
    - Свойство CurrentForecastDto.City теперь получается из Location;

--- Nubrio.Application/Services/WeatherForecastService.cs
+++ Nubrio.Application/Services/WeatherForecastService.cs
@@ -10,48 +10,58 @@ public class WeatherForecastService : IWeatherForecastService
 {
     private readonly IWeatherProvider _weatherProvider;
     private readonly IGeocodingService _geocodingService;
-    
-    public WeatherForecastService(IWeatherProvider weatherProvider, IGeocodingService geocodingService)
+    private readonly IClock _clock;
+    private readonly IConditionStringMapper _conditionStringMapper;
+
+    public WeatherForecastService(
+        IWeatherProvider weatherProvider,
+        IGeocodingService geocodingService,
+        IClock clock,
+        IConditionStringMapper conditionStringMapper)
     {
         _weatherProvider = weatherProvider;
         _geocodingService = geocodingService;
+        _clock = clock;
+        _conditionStringMapper = conditionStringMapper;
     }
 
     public async Task<Result<CurrentForecastDto>> GetCurrentForecastAsync(string city,
         CancellationToken cancellationToken)
     {
-        var location = await _geocodingService.ResolveAsync(city,  cancellationToken);
-        
-        var fetchResult = await _weatherProvider.GetCurrentForecastAsync(location.Value, cancellationToken);
+        if (string.IsNullOrWhiteSpace(city.Trim()))
+            return Result.Fail("City must not be empty or whitespace");
 
-        if (fetchResult.IsSuccess)
+        var geocodingResult = await _geocodingService.ResolveAsync(city, cancellationToken);
+
+        if (!geocodingResult.IsSuccess)
+            return Result.Fail(geocodingResult.Errors);
+
+        var fetchResult = await _weatherProvider.GetCurrentForecastAsync(geocodingResult.Value, cancellationToken);
+
+        if (fetchResult.IsFailed)
+            return Result.Fail(fetchResult.Errors);
+
+        var result = new CurrentForecastDto
         {
-            var result = new CurrentForecastDto
-            {
-                City = city,
-                Date = fetchResult.Value.ObservedAt,
-                Condition = fetchResult.Value.Condition.ToString(),
-                Temperature = fetchResult.Value.Temperature,
-                FetchedAt = DateTimeOffset.Now
-            };
-            
-            return Result.Ok(result);
-        }
+            City = geocodingResult.Value.Name,
+            Date = fetchResult.Value.ObservedAt,
+            Condition = _conditionStringMapper.From(fetchResult.Value.Condition),
+            Temperature = fetchResult.Value.Temperature,
+            FetchedAt = _clock.UtcNow
+        };
 
-        return Result.Fail(fetchResult.Errors);
+        return Result.Ok(result);
     }
 
-    public Task<Result<DailyForecastDto>> GetDailyForecastByDateAsync(Coordinates coordinates, DateOnly date,
+    public Task<Result<DailyForecastDto>> GetDailyForecastByDateAsync(string city, DateOnly date,
         CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
 
-    public Task<Result<DailyForecastDto>> GetDailyForecastRangeAsync(Coordinates coordinates,
+    public Task<Result<DailyForecastDto>> GetDailyForecastRangeAsync(string city,
         DateOnly startDate, DateOnly endDate, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
-
-    
 }
