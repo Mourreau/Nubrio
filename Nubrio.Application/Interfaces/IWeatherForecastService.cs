5e32493cbb75dcc62a78ee485e83be439c4464e1
m.moreau <max.mourreau@yandex.ru>
2025-10-06 16:34:02 +0500

    refactor(app): Изменить тип возвращаемого значения у методов сервиса IWeatherForecastService (#5).

--- Nubrio.Application/Interfaces/IWeatherForecastService.cs
+++ Nubrio.Application/Interfaces/IWeatherForecastService.cs
@@ -1,15 +1,18 @@
 using FluentResults;
+using Nubrio.Application.DTOs.CurrentForecast;
+using Nubrio.Application.DTOs.DailyForecast;
 using Nubrio.Domain.Models;
 
 namespace Nubrio.Application.Interfaces;
 
 public interface IWeatherForecastService
 {
-    Task<Result<CurrentForecast>> GetCurrentForecastAsync(string city);
+    Task<Result<CurrentForecastResponseDto>> GetCurrentForecastAsync(string city);
     
-    Task<Result<DailyForecast>> GetDailyForecastByDateAsync(Coordinates coordinates, DateOnly date);
+    Task<Result<DailyForecastResponseDto>> GetDailyForecastByDateAsync(Coordinates coordinates, DateOnly date);
     
-    Task<Result<DailyForecast>> GetDailyForecastByStartEndDateAsync(Coordinates coordinates, DateOnly startDate,  DateOnly endDate);
+    Task<Result<DailyForecastResponseDto>> GetDailyForecastByStartEndDateAsync(
+        Coordinates coordinates, DateOnly startDate,  DateOnly endDate);
     
     
     
