8e8efe3754df48ab5ed5c0e68d1bfdf9ac724cfe
m.moreau <max.mourreau@yandex.ru>
2025-10-13 15:31:35 +0500

    feat(infra): Добавить кеш в OpenMeteoConditionStringMapper(#7).

--- Nubrio.Infrastructure/Services/OpenMeteoConditionStringMapper.cs
+++ Nubrio.Infrastructure/Services/OpenMeteoConditionStringMapper.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using System.Text.RegularExpressions;
 using Nubrio.Application.Interfaces;
 using Nubrio.Domain.Enums;
@@ -6,11 +7,21 @@ namespace Nubrio.Infrastructure.Services;
 
 public class OpenMeteoConditionStringMapper : IConditionStringMapper
 {
+    private static readonly ConcurrentDictionary<WeatherConditions, string> _stringWmoCache = new();
+        
     public string From(WeatherConditions condition)
     {
+        // Проверяем есть ли значение в кеше
+        if (_stringWmoCache.TryGetValue(condition, out var cachedValue))
+            return cachedValue;
+        
         var name = condition.ToString();
 
         // Разделяем большие буквы пробелами: "PartlyCloudy" → "Partly Cloudy"
-        return Regex.Replace(name, "(?<!^)([A-Z])", " $1");
+        var readable =  Regex.Replace(name, "(?<!^)([A-Z])", " $1").ToLower();
+        
+        // Кладем в кеш и возвращаем строку
+        _stringWmoCache[condition] = readable;
+        return readable;
     }
 }
