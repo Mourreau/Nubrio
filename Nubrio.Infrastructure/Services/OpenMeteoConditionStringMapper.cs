5fb164ec17bf8a24a471168130e327e1861566a7
m.moreau <max.mourreau@yandex.ru>
2025-10-15 17:22:04 +0500

    refactor(infra): Добавить логирование (#15).

--- Nubrio.Infrastructure/Services/OpenMeteoConditionStringMapper.cs
+++ Nubrio.Infrastructure/Services/OpenMeteoConditionStringMapper.cs
@@ -1,27 +1,43 @@
 using System.Collections.Concurrent;
 using System.Text.RegularExpressions;
+using Microsoft.Extensions.Logging;
 using Nubrio.Application.Interfaces;
 using Nubrio.Domain.Enums;
 
 namespace Nubrio.Infrastructure.Services;
 
-public class OpenMeteoConditionStringMapper : IConditionStringMapper
+public partial class OpenMeteoConditionStringMapper : IConditionStringMapper
 {
     private static readonly ConcurrentDictionary<WeatherConditions, string> _stringWmoCache = new();
-        
+    private readonly ILogger<OpenMeteoConditionStringMapper> _logger;
+
+    public OpenMeteoConditionStringMapper(ILogger<OpenMeteoConditionStringMapper> logger)
+    {
+        _logger = logger;
+    }
+
     public string From(WeatherConditions condition)
     {
         // Проверяем есть ли значение в кеше
         if (_stringWmoCache.TryGetValue(condition, out var cachedValue))
+        {
+            _logger.LogDebug("Weather condition string: {Condition} already cached", condition);
             return cachedValue;
-        
+        }
+
+        _logger.LogDebug("Weather condition string: {Condition} is not cached", condition);
+
         var name = condition.ToString();
 
         // Разделяем большие буквы пробелами: "PartlyCloudy" → "Partly Cloudy"
-        var readable =  Regex.Replace(name, "(?<!^)([A-Z])", " $1").ToLowerInvariant();
-        
+        var readable = MyRegex().Replace(name, " $1").ToLowerInvariant();
+
         // Кладем в кеш и возвращаем строку
         _stringWmoCache[condition] = readable;
+        _logger.LogDebug("Weather condition string: {Condition} is cached", condition);
         return readable;
     }
+
+    [GeneratedRegex("(?<!^)([A-Z])")]
+    private static partial Regex MyRegex();
 }
