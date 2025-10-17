65cfc5009e2d4a5c0b6fcb3133016f3be2239611
m.moreau <max.mourreau@yandex.ru>
2025-10-17 20:23:55 +0500

    refactor(api): Убрать свойства и сделать primary-constructor в CurrentWeatherResponseDto.

--- Nubrio.Presentation/DTOs/Response/CurrentWeatherResponseDto.cs
+++ Nubrio.Presentation/DTOs/Response/CurrentWeatherResponseDto.cs
@@ -1,30 +1,11 @@
 namespace Nubrio.Presentation.DTOs.Response;
 
-public record CurrentWeatherResponseDto
-{
-    public CurrentWeatherResponseDto(
-        string city,
-        DateOnly date,
-        string condition,
-        double temperature,
-        string source,
-        DateTimeOffset fetchedAt,
-        string iconUrl = "Icon has not found")
-    {
-        City = city;
-        Date = date;
-        Condition = condition;
-        Temperature = temperature;
-        IconUrl = iconUrl;
-        Source = source;
-        FetchedAt = fetchedAt;
-    }
-    public string City { get;}
-    public DateOnly Date { get; }
-    public string Condition { get; }
-    public double Temperature { get; }
-    public string IconUrl { get; }
-    public string Source { get; }
-    public DateTimeOffset FetchedAt{ get; }
-}
+public record CurrentWeatherResponseDto(
+    string City,
+    DateOnly Date,
+    string Condition,
+    double Temperature,
+    string Source,
+    DateTimeOffset FetchedAt,
+    string IconUrl = "Icon has not found");
 
