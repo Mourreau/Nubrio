33b25f1143dc4880815c04c9739e0093db467e07
m.moreau <max.mourreau@yandex.ru>
2025-10-24 14:03:38 +0500

    feat(infra): Реализовать провайдер получения погоды OpenMeteo (#19):
    - GetDailyForecastMeanAsync() получение средней погоды за сутки по дате.

--- Nubrio.Infrastructure/OpenMeteo/OpenMeteoWeatherProvider.cs
+++ Nubrio.Infrastructure/OpenMeteo/OpenMeteoWeatherProvider.cs
@@ -1,19 +1,91 @@
 using FluentResults;
 using Nubrio.Application.Interfaces;
 using Nubrio.Domain.Models;
+using Nubrio.Domain.Models.Daily;
+using Nubrio.Infrastructure.Http;
+using Nubrio.Infrastructure.OpenMeteo.DTOs.DailyForecast.MeanForecast;
+using Nubrio.Infrastructure.OpenMeteo.Validators;
 
 namespace Nubrio.Infrastructure.OpenMeteo;
 
 public class OpenMeteoWeatherProvider : IWeatherProvider
 {
-    public Task<Result<DailyForecast>> GetDailyForecastRangeAsync(Location location, DateOnly startDate, DateOnly endDate,
-        CancellationToken cancellationToken)
+    private readonly IWeatherCodeTranslator _weatherCodeTranslator;
+    private readonly IOpenMeteoClient _client;
+
+    private int WeatherElements { get; set; }
+
+    public OpenMeteoWeatherProvider(IOpenMeteoClient client, IWeatherCodeTranslator weatherCodeTranslator)
     {
-        throw new NotImplementedException();
+        _weatherCodeTranslator = weatherCodeTranslator;
+        _client = client;
     }
 
-    public Task<Result<CurrentForecast>> GetCurrentForecastAsync(Location location, CancellationToken cancellationToken)
+    public async Task<Result<DailyForecastMean>> GetDailyForecastMeanAsync(Location location, DateOnly date,
+        CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        
+
+        var clientResponse = await _client.GetOpenMeteoDailyMeanAsync(
+            location.Coordinates.Latitude,
+            location.Coordinates.Longitude,
+            date,
+            cancellationToken);
+        
+        
+        if (clientResponse.IsFailed)
+            return Result.Fail(clientResponse.Errors);
+
+        var openMeteoResponseDto = clientResponse.Value;
+
+        var validationResult = OpenMeteoResponseValidator.ValidateMean(openMeteoResponseDto);
+        if (validationResult.IsFailed)
+        {
+            var error = new Error(validationResult.Message);
+            if(!string.IsNullOrEmpty(validationResult.Code))
+                error = error.WithMetadata("Code", validationResult.Code);
+            
+            return Result.Fail(error);
+        }
+
+        WeatherElements = validationResult.WeatherElements;
+        
+        if (WeatherElements != 1) return Result.Fail("Weather elements out of range.Expected 1 element.");
+
+        var result = MapToDomainModelDailyForecastMean(openMeteoResponseDto,  location);
+        
+        if (result.IsFailed) return Result.Fail<DailyForecastMean>(result.Errors);
+        
+
+        return Result.Ok(result.Value);
+    }
+
+    public async Task<Result<CurrentForecast>> GetCurrentForecastAsync(Location location, CancellationToken cancellationToken)
+    {
+        return Result.Fail($"{nameof(GetCurrentForecastAsync)} is not implemented");
+    }
+
+    
+    
+    
+    // -------------------------------------------------------------------------------------------------------------- //
+    private Result<DailyForecastMean> MapToDomainModelDailyForecastMean(
+        OpenMeteoDailyMeanResponseDto openMeteoResponseDto, Location location)
+    {
+        const int index = 0;
+        var dateTranslate = DateOnly.Parse(
+            openMeteoResponseDto.Daily.Time[index]); // Здесь конкретно указан [0] элемент листа т.к. в данном контексте дата будет только одна.
+        
+        
+        var dailyForecastResult = new DailyForecastMean
+        (
+            dateTranslate,
+            location.LocationId,
+            _weatherCodeTranslator.Translate(openMeteoResponseDto.Daily.WeatherCode[index]),
+            openMeteoResponseDto.Daily.Temperature2mMean[index]
+            // Берем [0] элемент листа. Прогноз на одну дату и значение в листе тоже будет одно.
+        );
+
+        return Result.Ok(dailyForecastResult);
     }
 }
