5c298f752d946786ef3058a02735f20228781ff1
m.moreau <max.mourreau@yandex.ru>
2025-11-10 22:30:00 +0500

    test: Реализовать заглушку для интеграционных тестов клиента

---
+++ Nubrio.Tests/Infrastructure/IntegrationTests/Http/StubHttpMessageHandler.cs
@@ -0,0 +1,20 @@
+namespace Nubrio.Tests.Infrastructure.IntegrationTests.Http;
+
+internal sealed class StubHttpMessageHandler : HttpMessageHandler
+{
+    private readonly Func<HttpRequestMessage, int, HttpResponseMessage> _responder;
+    public int Calls { get; private set; }
+
+    public StubHttpMessageHandler(Func<HttpRequestMessage, int, HttpResponseMessage> responder)
+    {
+        _responder = responder;
+    }
+    
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        Calls++;
+        var response = _responder(request, Calls);
+        return Task.FromResult(response);
+    }
+}
