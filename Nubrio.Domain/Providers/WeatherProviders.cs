6bcb1e07cb521247d7a6ae97730e930f9371468a
m.moreau <max.mourreau@yandex.ru>
2025-11-10 22:29:31 +0500

    feat(dom): Реализовать класс провайдеров содержащий строковое значение названия провайдера (#22)

---
+++ Nubrio.Domain/Providers/WeatherProviders.cs
@@ -0,0 +1,8 @@
+namespace Nubrio.Domain.Providers;
+
+public static class WeatherProviders
+{
+    public const string OpenMeteoForecast = "openmeteo.forecast";
+    public const string OpenMeteoGeocoding = "openmeteo.geocoding";
+    
+}
