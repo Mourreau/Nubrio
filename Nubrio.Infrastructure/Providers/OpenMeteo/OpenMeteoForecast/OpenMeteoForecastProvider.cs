820d37269d9747a45411f25c582c8f2ebcb13798
m.moreau <max.mourreau@yandex.ru>
2025-11-28 12:13:23 +0500

    refactor(infra/openmeteo): Обновить провайдеры под новых HTTP-клиентов:
    - Переименован OpenMeteoWeatherProvider в OpenMeteoForecastProvider
    - Обновлён OpenMeteo***Provider под интерфейсы IGeocodingClient / IForecastClient

--- Nubrio.Infrastructure/Providers/OpenMeteo/OpenMeteoForecast/OpenMeteoForecastProvider.cs
+++ Nubrio.Infrastructure/Providers/OpenMeteo/OpenMeteoForecast/OpenMeteoForecastProvider.cs
@@ -3,19 +3,19 @@ using FluentResults;
 using Nubrio.Application.Interfaces;
 using Nubrio.Domain.Models;
 using Nubrio.Domain.Models.Daily;
-using Nubrio.Infrastructure.Http.ForecastClient;
-using Nubrio.Infrastructure.OpenMeteo.DTOs.DailyForecast.MeanForecast;
-using Nubrio.Infrastructure.OpenMeteo.Validators;
+using Nubrio.Infrastructure.Clients.ForecastClient;
+using Nubrio.Infrastructure.Providers.OpenMeteo.DTOs.DailyForecast.MeanForecast;
+using Nubrio.Infrastructure.Providers.OpenMeteo.Validators;
 
 namespace Nubrio.Infrastructure.Providers.OpenMeteo.OpenMeteoForecast;
 
-public class OpenMeteoWeatherProvider : IWeatherProvider
+public class OpenMeteoForecastProvider : IForecastProvider
 {
     private readonly IWeatherCodeTranslator _weatherCodeTranslator;
     private readonly IForecastClient _client;
 
 
-    public OpenMeteoWeatherProvider(IForecastClient client, IWeatherCodeTranslator weatherCodeTranslator)
+    public OpenMeteoForecastProvider(IForecastClient client, IWeatherCodeTranslator weatherCodeTranslator)
     {
         _weatherCodeTranslator = weatherCodeTranslator;
         _client = client;
@@ -24,15 +24,13 @@ public class OpenMeteoWeatherProvider : IWeatherProvider
     public async Task<Result<DailyForecastMean>> GetDailyForecastMeanAsync(Location location, DateOnly date,
         CancellationToken cancellationToken)
     {
-        
-
         var clientResponse = await _client.GetOpenMeteoDailyMeanAsync(
             location.Coordinates.Latitude,
             location.Coordinates.Longitude,
             date,
             cancellationToken);
-        
-        
+
+
         if (clientResponse.IsFailed)
             return Result.Fail(clientResponse.Errors);
 
@@ -42,41 +40,40 @@ public class OpenMeteoWeatherProvider : IWeatherProvider
         if (validationResult.IsFailed)
         {
             var error = new Error(validationResult.Message);
-            if(!string.IsNullOrEmpty(validationResult.Code))
+            if (!string.IsNullOrEmpty(validationResult.Code))
                 error = error.WithMetadata("Code", validationResult.Code);
-            
+
             return Result.Fail(error);
         }
-        
-        
-        if (validationResult.WeatherElements != 1) 
+
+
+        if (validationResult.WeatherElements != 1)
             return Result.Fail("Weather elements out of range. Expected 1 element.");
 
-        var result = MapToDomainModelDailyForecastMean(openMeteoResponseDto,  location);
+        var result = MapToDomainModelDailyForecastMean(openMeteoResponseDto, location);
 
         return Result.Ok(result);
     }
 
-    public async Task<Result<CurrentForecast>> GetCurrentForecastAsync(Location location, CancellationToken cancellationToken)
+    public async Task<Result<CurrentForecast>> GetCurrentForecastAsync(Location location,
+        CancellationToken cancellationToken)
     {
         return Result.Fail($"{nameof(GetCurrentForecastAsync)} is not implemented");
     }
 
-    
-    
-    
+
     // -------------------------------------------------------------------------------------------------------------- //
     private DailyForecastMean MapToDomainModelDailyForecastMean(
         OpenMeteoDailyMeanResponseDto openMeteoResponseDto, Location location)
     {
         // Здесь конкретно указан [0] элемент листа т.к. в данном контексте элемент будет только один.
         const int index = 0;
-        
+
         var dateTranslate = DateOnly.Parse(
-            openMeteoResponseDto.Daily.Time[index], 
-            CultureInfo.InvariantCulture); 
-        
-        
+            openMeteoResponseDto.Daily.Time[index],
+            CultureInfo.InvariantCulture);
+
+
         var dailyForecastResult = new DailyForecastMean
         (
             dateTranslate,
--- Nubrio.Infrastructure/Providers/OpenMeteo/OpenMeteoGeocoding/OpenMeteoGeocodingProvider.cs
+++ Nubrio.Infrastructure/Providers/OpenMeteo/OpenMeteoGeocoding/OpenMeteoGeocodingProvider.cs
@@ -1,27 +1,30 @@
 using FluentResults;
 using Nubrio.Application.Interfaces;
+using Nubrio.Domain.Enums;
 using Nubrio.Domain.Models;
-using Nubrio.Infrastructure.Http.GeocodingClient;
-using Nubrio.Infrastructure.Options;
+using Nubrio.Infrastructure.Clients.GeocodingClient;
 
 namespace Nubrio.Infrastructure.Providers.OpenMeteo.OpenMeteoGeocoding;
 
-public class OpenMeteoGeocodingProvider : IGeocodingProvider
+internal sealed class OpenMeteoGeocodingProvider : IGeocodingProvider
 {
     private readonly IGeocodingClient _geocodingClient;
     private const int CityCount = 1;
+
 
     public OpenMeteoGeocodingProvider(IGeocodingClient geocodingClient)
     {
         _geocodingClient = geocodingClient;
     }
 
+
     public async Task<Result<Location>> ResolveAsync(string city, string language, CancellationToken cancellationToken)
     {
         if (string.IsNullOrWhiteSpace(city))
             return Result.Fail("City cannot be null or whitespace");
 
-        var openMeteoGeoResponse = await _geocodingClient.GeocodeAsync(city, CityCount, language, cancellationToken);
+        var openMeteoGeoResponse =
+            await _geocodingClient.GeocodeAsync(city, CityCount, language, cancellationToken);
 
         if (openMeteoGeoResponse.IsFailed)
             return Result.Fail(openMeteoGeoResponse.Errors);
