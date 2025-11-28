8ba04a072ea728c6c15de9479309ec5c89525e40
m.moreau <max.mourreau@yandex.ru>
2025-11-28 12:15:24 +0500

    feat(infra/options): Добавить ProviderOptions и настройки OpenMeteo
    - Добавлен OpenMeteoProviderInfo с именем и базовыми URL
    - Добавлен ProviderSettings для конфигурации таймаутов и политики повторов
    - Добавлен ProviderOptions как корневая точка для options провайдеров
    - Удален OpenMeteoProviderInfo

--- Nubrio.Infrastructure/Options/OpenMeteoProviderInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace Nubrio.Infrastructure.Options;
-
-public static class OpenMeteoProviderInfo
-{
-    public const string OpenMeteoForecast = "openmeteo.forecast";
-    public const string OpenMeteoGeocoding = "openmeteo.geocoding";
-    
-}
